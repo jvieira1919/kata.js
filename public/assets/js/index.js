c9a3149a98b10310e534bda2a02ba1bce7d1363b
kamalwc <carmichaelkw@gmail.com>
2020-06-21 10:42:20 -0400

    added eslint

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,16 +1,16 @@
 /**
  * Google books API Docs: https://developers.google.com/books/docs/v1/using#auth
- * 
+ *
  */
 
 //endpoint returns the data on 17 thriller books
 function getBooks(){
-    let preference = 'Thriller';
-    let max = 17; // up to 40 
+  let preference = "Thriller",
+    max = 17; // up to 40
 
-    $.get(`https://www.googleapis.com/books/v1/volumes?q=${preference}:fiction&maxResults=${max}`, function(data){
-        console.log(data.items);//array of objects 
-    })
+  $.get(`https://www.googleapis.com/books/v1/volumes?q=${preference}:fiction&maxResults=${max}`, function(data){
+    console.log(data.items);//array of objects
+  });
 }
 
 getBooks();
