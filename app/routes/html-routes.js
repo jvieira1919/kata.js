1203394f1e63e452e8eb78f37267cb48a56bc31c
kamalwc <carmichaelkw@gmail.com>
2020-06-22 20:03:57 -0400

    added route to server the home page

---
+++ app/routes/html-routes.js
@@ -0,0 +1,8 @@
+var path = require("path");
+// const __dirname = path.resolve();
+
+module.exports = function (app) {
+  app.get("/", function (req, res) {
+    res.sendFile("index.html",{root: path.join(__dirname,"../public/views")});
+  });
+};
