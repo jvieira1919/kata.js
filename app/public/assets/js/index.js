ab7e837508ebab0bdd1466214d7b833e8264f396
kamalwc <carmichaelkw@gmail.com>
2020-06-24 02:49:39 -0400

    configuring google api consumption

--- app/public/assets/js/index.js
+++ app/public/assets/js/index.js
@@ -3,42 +3,69 @@
  *
  */
 
-//endpoint returns the data on 17 thriller books
-function getBooks(){
+function getBooks() {
   let preference = "Thriller",
     max = 17; // up to 40
 
 
 
-  $.get(`https://www.googleapis.com/books/v1/volumes?q=${preference}:fiction&maxResults=${max}`, function(data){
+  $.get(`https://www.googleapis.com/books/v1/volumes?q=${preference}:fiction&maxResults=${max}`, function (data) {
     console.log(data.items);//array of objects
 
     const books = data.items;
 
-      // let title = data.items[6].volumeInfo.title,
-      //     cover = data.items[6].volumeInfo.imageLinks;
+    books.forEach(book => {
 
-      //     console.log(title);
-      //     console.log(cover);
+      let title = book.volumeInfo.title,
+        cover = book.volumeInfo.imageLinks.smallThumbnail,
+        // isbn = book.volumeInfo.industryIdentifiers[1].identifier,
+        authors = book.volumeInfo.authors[0],
+        summary = book.volumeInfo.description;
 
-          
-    // books.forEach(book =>{
-    //   let div = $('<div></div'),
-    //       img = $('<img>'),
-    //       p = $('<p><p>');
+      let div = $(`<div class='parent'></div>`),
+        img = $(`<img>`),
+        titlep = $(`<p id="title">${title}<p>`),
+        // isbnp = $(`<p id="isbn">${isbn}<p>`),
+        authorsp = $(`<p id="authors">${authors}<p>`),
+        summaryp = $(`<p id="summary">${summary}<p>`),
+        add = $(`<button>add</button>`);
 
-    //   let title = book.volumeInfo.title,
-    //       cover = book.volumeInfo.imageLinks;
-    //   img.attr('src', cover);
-    //   p.html(title);
+      img.attr('src', cover);
 
-    //   div.append(img);
-    //   div.append(p);
+      div.append(img);
+      div.append(titlep);
+      // div.append(isbnp);
+      div.append(authorsp);
+      div.append(summaryp);
+      div.append(add);
 
-    //   $('start').append(div);
-    // })
-
+      $('#start').append(div);
+    })
   });
 }
 
 getBooks();
+
+
+// log book data when you click on add button ( post prep)
+// create the layout foe the page 
+// get page to serve 
+//@JV create the post route  me 2 consume 
+
+$('.parent').on('click', function (event) {
+
+  // const obj = {
+  //   isbn: 
+  //   // authors: "",
+  //   // summary: "",
+  //   // id: "",
+  //   // title: event.target.parentElement.title
+  // }
+
+  if (event.target.matches("button")){
+    // console.log(event.target.parentElement.id);
+    console.log("success");
+
+  }
+    
+})
