2a344103cc20d062310d621761afa426fbc0119a
kamalwc <carmichaelkw@gmail.com>
2020-06-25 00:17:23 -0400

    add show and hide button to books card

--- app/public/assets/js/index.js
+++ app/public/assets/js/index.js
@@ -3,14 +3,85 @@
  *
  */
 
-//endpoint returns the data on 17 thriller books
-function getBooks(){
+ async function getBooks() {
   let preference = "Thriller",
-    max = 17; // up to 40
+    max = 17; 
 
-  $.get(`https://www.googleapis.com/books/v1/volumes?q=${preference}:fiction&maxResults=${max}`, function(data){
-    console.log(data.items);//array of objects
-  });
+  await $.get(`https://www.googleapis.com/books/v1/volumes?q=${preference}:fiction&maxResults=${max}`, function (data) {
+    // console.log(data.items);//array of objects
+
+    const books = data.items;
+  console.log(books);
+  
+    for (let i = 0; i < books.length; i++) {
+      let title = books[i].volumeInfo.title,
+        cover = books[i].volumeInfo.imageLinks.smallThumbnail,
+        // isbn = books[i].volumeInfo.industryIdentifiers[1].identifier,
+        authors = books[i].volumeInfo.authors[0],
+        summary = books[i].volumeInfo.description;
+
+      let div = $(`<div class='parent' data-result=${i}></div>`);
+      let = img = $(`<img>`);
+      titlep = $(`<p id="title">Title: ${title}<p>`),
+        // isbnp = $(`<p id="isbn">${isbn}<p>`),
+        authorsp = $(`<p id="authors">Author: ${authors}<p>`),
+        summaryp = $(`<p class="summary">Summary: ${summary}<p>`),
+        show = $(`<button class="show-summary">Show Summary</button>`),
+        add = $(`<button class="add">add</button>`);
+
+      img.attr('src', cover);
+
+      div.append(img);
+      div.append(titlep);
+      // div.append(isbnp);
+      div.append(authorsp);
+      div.append(show);
+      div.append(summaryp);
+      div.append(add);
+
+      $('#start').append(div);
+
+    }
+
+  })
+
+  $('p.summary').hide()
 }
 
 getBooks();
+
+// TO DO ***************
+// style main books  page 
+// get page to serve 
+// @JV create the post route  me 2 consume 
+// @JV create the Get route for our libraries table 4 libraries page
+
+
+$('#start').on('click', ".show-summary", function (event) {
+  if (event.target.matches(".show-summary")) {
+    $(event.target.parentElement.children[6]).toggle()
+  }
+})
+
+$('#start').on('click', "button.add", function (event) {
+  let title, author;
+  if (event.target.matches(".add")) {
+    title = event.target.parentElement.children[1].innerHTML;
+    author = event.target.parentElement.children[3].innerHTML;
+  }
+  // $.post();/// post to JV's endpoint 
+  const obj = {
+    // isbn: 
+    authors: author,
+    title: title,
+    singer: "james"
+  }
+
+  console.log(obj);
+})
+
+
+
+
+
+ 
